489772162dd2255199645a36d37ba03c1188d3e7
nikkho <nikkho@ddd8a7e7-04a1-4c0d-ae79-48073a28b335>
2016-07-26 16:13:59 +0000

    Improved already running instances detection
    git-svn-id: svn://svn.code.sf.net/p/nikkhokkho/code/trunk@508 ddd8a7e7-04a1-4c0d-ae79-48073a28b335

--- FileOptimizer/FileOptimizer.cpp
+++ FileOptimizer/FileOptimizer.cpp
@@ -30,8 +30,8 @@ int WINAPI _tWinMain(HINSTANCE phInstance, HINSTANCE phPrevInstance, LPTSTR pacC
 
 	try
 	{
-		hMutex = CreateMutex(NULL, false, Application->Name.c_str());
-		if (GetLastError() == ERROR_ALREADY_EXISTS)
+		hMutex = CreateMutex(NULL, true, Application->Name.c_str());
+		if ((!hMutex) || (GetLastError() == ERROR_ALREADY_EXISTS))
 		{
 			if (clsUtil::MsgBox(NULL, ("There is one instace of " + Application->Name + " still running.\r\n\r\nDo you want to open another?").c_str(), _T("Still running"), MB_YESNO | MB_ICONQUESTION) == IDNO)
 			{
