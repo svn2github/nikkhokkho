c73445112c9ada544f2f950b17763e6d2aa30e0e
nikkho <nikkho@ddd8a7e7-04a1-4c0d-ae79-48073a28b335>
2017-05-08 07:07:11 +0000

    Tweak SQLite optimization
    git-svn-id: svn://svn.code.sf.net/p/nikkhokkho/code/trunk@795 ddd8a7e7-04a1-4c0d-ae79-48073a28b335

--- FileOptimizer/Win64/Debug/Plugins64/sqlite.bat
+++ FileOptimizer/Win64/Debug/Plugins64/sqlite.bat
@@ -1,8 +1,8 @@
 @echo off
-del %1.sql > nul
-del %2 > nul
-sqlite3.exe %1 .dump > %1.sql
+del /f /q %1.sql > nul
+del /f /q %2 > nul
+echo PRAGMA optimize(0xfffe); > %1.sql
+sqlite3.exe %1 .dump >> %1.sql
 sqlite3.exe %2 < %1.sql
-del %1.sql > nul
-rem sqlite3.exe %2 "PRAGMA optimize(0xfffe); VACUUM;"
-del %1.sql > nul
+del /f /q %1.sql > nul
+del /f /q %1.sql > nul
