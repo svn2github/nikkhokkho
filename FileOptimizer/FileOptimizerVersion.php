15e063a43b17884331cde3bd9006226c10f54506
nikkho <nikkho@ddd8a7e7-04a1-4c0d-ae79-48073a28b335>
2018-09-10 08:08:44 +0000

    13.20
    git-svn-id: svn://svn.code.sf.net/p/nikkhokkho/code/trunk@1378 ddd8a7e7-04a1-4c0d-ae79-48073a28b335

--- FileOptimizer/FileOptimizerVersion.php
+++ FileOptimizer/FileOptimizerVersion.php
@@ -76,7 +76,7 @@
 		'ShowToolBar' => false,
 		'HideAds' => false,
 		'TempDirectory' => '',
-		'Version' => '13.10.2345',
+		'Version' => '13.20.2377',
 		'Time' => 0,
 		'Opens' => 0,
 		'Files' => 0,
